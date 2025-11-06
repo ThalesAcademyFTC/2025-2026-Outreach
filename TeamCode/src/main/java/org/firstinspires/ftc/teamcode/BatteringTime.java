4a580d16fd8cb0ba387aa6d5cf6dacd8dd647b49
RyanP2005 <ryan.patel@students.thalesacadmey.org>
2025-11-06 14:33:18 -0500

    im gonna kill everybody. im starting with ryan, tho :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BatteringTime.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BatteringTime.java
@@ -1,64 +1,92 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import static java.lang.Thread.sleep;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.IMU;
-
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
 public class BatteringTime extends OpMode {
-    public driverCentricMovement driverCentricMovement;
+    DcMotor leftRear;
+    DcMotor rightRear;
+    DcMotorEx ramMotor;
 
-    public driverCentricMovement fieldCentricMovement;
-
-    public mainLibrary mainLibrary;
-
-    double rbtSpd = .5;
-
-    double startPosition = .5;
-
-    double rgbPos = .277;
-
-    double keep = 0;
-
-    double start = 0;
+    boolean retracting = false;
+    boolean retracted = false;
+    final int LIFT_POSITION = 0;
+    final int RELEASE_POSITION = 2000;
 
     public void init() {
 
-        mainLibrary = new mainLibrary(this, org.firstinspires.ftc.teamcode.mainLibrary.Drivetrain.TANK);
+        leftRear = hardwareMap.get(DcMotor.class, "leftRear");
+        leftRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRear = hardwareMap.get(DcMotor.class, "rightRear");
+        rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        ramMotor = hardwareMap.get(DcMotorEx.class, "ramMotor");
+        ramMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        driverCentricMovement = new driverCentricMovement(mainLibrary);
-
-        driverCentricMovement = new driverCentricMovement(mainLibrary);
-
-        mainLibrary = new mainLibrary(this, org.firstinspires.ftc.teamcode.mainLibrary.Drivetrain.TANK);
-        IMU imu = mainLibrary.hwMap.get(IMU.class, "imu");
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
-        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-        imu.initialize(parameters);
-
-
-        telemetry.addData("Touch sensor state", mainLibrary.touchSensor.isPressed());
-        telemetry.addData("Distance from distance sensor", mainLibrary.distanceSensor.getDistance(DistanceUnit.CM));
-        telemetry.addData("Red value", mainLibrary.colorSensor.red());
-        telemetry.addData("Blue value", mainLibrary.colorSensor.blue());
-        telemetry.addData("Green value", mainLibrary.colorSensor.green());
-        telemetry.addData("Alpha value", mainLibrary.colorSensor.alpha());
+        // lift ram until it stops and reset/zero the motor position
+        ramMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        ramMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        int newPosition = -10000;
+        int position = ramMotor.getCurrentPosition();
+        ramMotor.setPower(-0.1);
+        while(Math.abs(newPosition - position) < 2) {
+            position = newPosition;
+            try {
+                sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            newPosition = ramMotor.getCurrentPosition();
+        }
+        ramMotor.setPower(0);
+        ramMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ramMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        retracted = true;
     }
+
     public void loop() {
 
-        double y = (-gamepad1.left_stick_y);
-        double x = (gamepad1.left_stick_x);
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        drive(x, y);
 
-        driverCentricMovement.driverMovement(x, y);
+        if(!retracting && gamepad1.aWasPressed()) {
+            retracting = true;
+            liftRam();
+        }
+        if(!retracting && gamepad1.rightBumperWasPressed()) {
+            releaseRam();
+            retracted = false;
+        }
+        if(retracting) {
+            if(Math.abs(ramMotor.getCurrentPosition() - LIFT_POSITION) < 3) {
+                retracting = false;
+                retracted = true;
+            }
+        }
+    }
 
+    public void liftRam() {
+        ramMotor.setTargetPosition(LIFT_POSITION);
+    }
 
-
-
-
+    public void releaseRam() {
+        ramMotor.setTargetPosition(RELEASE_POSITION);
+    }
+    public void drive(double x, double y) {
+        // y=1, x=0  forward
+        // y=0, x=1  turn clockwise in place
+        // y=1, x=1  turn to the right while moving 'forward'
+        double leftPower = Range.clip(x + y, -1.0, 1.0);
+        double rightPower = Range.clip(x - y, -1.0, 1.0);
+        rightRear.setPower(rightPower);
+        leftRear.setPower(leftPower);
     }
 }
